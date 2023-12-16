efa1d42af21eebf48c85a58853975104b5463961
Kuba Walat <kubawalat@gmail.com>
2023-12-16 11:52:42 +0100

    feat(setup): :tada: initialize spring boot project

---
+++ src/main/java/pl/qbawalat/filerelationsresolver/FileRelationsResolverApplication.java
@@ -0,0 +1,13 @@
+package pl.qbawalat.filerelationsresolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileRelationsResolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileRelationsResolverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/qbawalat/filerelationsresolver/FileRelationsResolverApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.qbawalat.filerelationsresolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileRelationsResolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
