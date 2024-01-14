47c60a3145d1877dbfe559a292ac3f430ce4831e
Kuba Walat <kubawalat@gmail.com>
2024-01-14 19:25:53 +0100

    feat: :sparkles: add file name format validation

--- src/main/java/pl/qbawalat/file/relations/resolver/api/file/validator/MultipartFileValidator.java
+++ src/main/java/pl/qbawalat/file/relations/resolver/api/file/validator/MultipartFileValidator.java
@@ -3,6 +3,7 @@ package pl.qbawalat.file.relations.resolver.api.file.validator;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import java.util.Arrays;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,11 +18,21 @@ public class MultipartFileValidator implements ConstraintValidator<FileTypeConst
 
     @Override
     public boolean isValid(MultipartFile multipartFile, ConstraintValidatorContext constraintValidatorContext) {
+        return isValidFileName(Objects.requireNonNull(multipartFile.getOriginalFilename()))
+                && hasMatchingFileTypes(multipartFile);
+    }
+
+    private Boolean hasMatchingFileTypes(MultipartFile multipartFile) {
         return requestParamsReader
                 .getFileType()
                 .map(expectedFileType -> Arrays.stream(ConsumeableFileType.values())
                                 .anyMatch(val -> val.getMimeType().equalsIgnoreCase(expectedFileType))
                         && expectedFileType.equalsIgnoreCase(multipartFile.getContentType()))
                 .orElse(false);
+    }
+
+    private boolean isValidFileName(String input) {
+        int lastDotIndex = input.lastIndexOf('.');
+        return lastDotIndex > 0 && input.charAt(lastDotIndex - 1) == 's';
     }
 }
